fa7c576690db936385f937c4da1b2ea5559fd06e
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2022-11-17 18:58:00 +0530

    17nov

--- Task/16Nov.js
+++ Task/16Nov.js
@@ -7,7 +7,7 @@ setTimeout(() => {
 	console.log('TimeOut 1');
 }, 500);
 setTimeout(() => {
-	Promise.resolve().then((a) => console.log('Promise 2'));
+	Promise.resolve().then((a) => console.log('Promise 2', a));
 	setTimeout(() => {
 		Promise.resolve().then((a) => console.log('Promise 3'));
 	}, 0);
@@ -20,3 +20,12 @@ console.log('Hello 5');
 console.log('Bye!');
 
 //! ------------------------------------------after Noon-----------------------------------------------
+
+setTimeout(() => {
+	Promise.resolve().then((a) => console.log('Resolved!'));
+}, 1000);
+setTimeout(() => {
+	Promise.reject((a) => {
+		console.log('Reject!!');
+	});
+}, 1000);
---
+++ Task/17nov.js
@@ -0,0 +1,20 @@
+const countryData = (name) => {
+	const data = fetch(`https://restcountries.com/v3.1/name/${name}/`)
+		.then((data) => {
+			if (!data.ok) throw new Error('Something Went wrong');
+			data.json();
+		})
+		.then((country) => console.log(country[0].borders));
+};
+
+const countryDataByAlpha = (name) => {
+	const data = fetch(`https://restcountries.com/v2/alpha/${name}/`)
+		.then((data) => {
+			if (!data.ok) throw new Error('Something Went wrong');
+			data.json();
+		})
+		.then((country) => console.log(country));
+};
+
+countryData('india');
+countryDataByAlpha('in');
