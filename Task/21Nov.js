f9d1868bcd50991b3a0f9587cb4478ffb924a55c
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2022-11-23 11:50:03 +0530

    added extra hw

--- Task/21Nov.js
+++ Task/21Nov.js
@@ -1,6 +1,22 @@
 // // Quiz discussion
 // for(a)
+// ! ------------------------------ Morning ------------------------------------------------------
 
+// ! Memory life cycle
+// => Allocate memory
+// => Use memory
+// => Release memory
+
+//                                    The memory heap and stack
+//!               Stack                                                     	Heap
+
+//   Primitive values and references	                                Objects and functions
+//   Size is known at compile time	                                  Size is known at run time
+//   Allocates a fixed amount of memory	                              No limit per object
+
+//                                 References in JavaScript
+//====>> JavaScript stores objects and functions in the heap.
+//====>> Primitive values and references are stored in the stack.
 //! --------------------------------------AfterNoon----------------------------------------------
 
 // async & await used to return a promise
@@ -14,7 +30,7 @@ async function b() {
 }
 
 // b();
-// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=lat&longitude=long&localityLanguage=en
+//  https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=lat&longitude=long&localityLanguage=en
 
 // https://reqres.in/api/users
 // async function get() {
@@ -33,4 +49,34 @@ async function data() {
 	}
 }
 
-data();
+// data();
+function locationa() {
+	let a = navigator.geolocation.getCurrentPosition((data) => {
+		let lete = data.coords.latitude;
+		let long = data.coords.latitude;
+	});
+	async function showPosition(lat, long) {
+		// let lat = position.coords.latitude;
+		// let long = position.coords.longitude;
+		try {
+			const data = await fetch(
+				`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`
+			);
+			if (!data.ok) throw new Error('something went wrong');
+			const user = await data.json();
+			console.log(`City: ${user.city}
+		Country: ${user.countryName} 
+		Locality :${user.locality}
+		State: ${user.principalSubdivision}`);
+			document.getElementById('Address').innerHTML = `City: ${user.city}
+		Country: ${user.countryName} 
+		Locality :${user.locality}
+		State: ${user.principalSubdivision}`;
+		} catch (err) {
+			console.log(err.message, 'hhhh');
+		}
+	}
+	showPosition(late, long);
+}
+
+locationa();
---
+++ Task/22Nov.js
@@ -0,0 +1,21 @@
+// default export
+// Named export
+// polyfill
+let name = 'hello';
+function greet() {
+	console.log('erger', this.name);
+	console.log('erger', this.name);
+	console.log('erger', this.name);
+}
+
+// let emp={name:"bye"}
+// emp
+// greet();
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Promise Combinators
+// Promise.all run the promise parallel
+// Promise.race?
+// Promise.
+// async and Defer
+// DOM
