fecf61875f5a78f52b0bdbee3063f9f150b01d10
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2022-11-23 20:00:44 +0530

    stopwatch

---
+++ Task/23Nov.js
@@ -0,0 +1,12 @@
+// !---------------------------------After Noon session ------------------------------------------
+
+// Intro html
+//! type of elements
+//>>  block element div all tags
+//>>  Inline element span
+
+//! Syenitic tag
+// which convey a meaning
+// help to creating assessable website
+//  help to create seo friendly website
+// example = form ,image ,button
