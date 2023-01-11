1f105644f087c233939738305051ae3a7185adab
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2023-01-11 10:52:06 +0530

    rfwef

--- React/Akshay-sir-Project/src/semantic-ui/Wishlist.js
+++ React/Akshay-sir-Project/src/semantic-ui/Wishlist.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-import Data from './Data';
-import Like from './Like';
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
+import Data from "./Data";
+import Like from "./Like";
 const Wishlist = () => {
 	return (
 		<div style={{ margin: 20 }}>
@@ -11,10 +11,10 @@ const Wishlist = () => {
 						<Grid.Row>
 							<Grid.Column>
 								<Like />
-								<Image style={{ width: 150, height: 'auto' }} src={item.image} />
+								<Image style={{ width: 150, height: "auto" }} src={item.image} />
 							</Grid.Column>
 							<Grid.Column>
-								<span style={{ color: 'green', fontSize: 20 }}>{item.name}</span>
+								<span style={{ color: "green", fontSize: 20 }}>{item.name}</span>
 								<ul>
 									<li>6 GB RAM 128 GB ROM Expandable Upto 1 TB</li>
 									<li>16.76 cm (6.6 inch) Full HD+ Display 50MP + 8MP 16MP</li>
@@ -24,11 +24,11 @@ const Wishlist = () => {
 							</Grid.Column>
 							<Grid.Column>
 								<span style={{ fontSize: 20 }}>
-									{'\u20B9'}
-									{item.price} <sup style={{ color: 'green', fontSize: 12 }}>2000 off</sup>
+									{"\u20B9"}
+									{item.price} <sup style={{ color: "green", fontSize: 12 }}>2000 off</sup>
 									<div style={{ fontSize: 15 }}>
 										<strike>
-											{'\u20B9'}
+											{"\u20B9"}
 											{item.price - 2000}
 										</strike>
 									</div>
